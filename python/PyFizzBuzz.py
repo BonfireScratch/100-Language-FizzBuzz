095e00abc4019271690f3b630763c5ef8417f905
SimpleBinary <40658215+SimpleBinary@users.noreply.github.com>
2018-12-02 06:29:33 +1000

    Update python to use for loop

--- python/PyFizzBuzz.py
+++ python/PyFizzBuzz.py
@@ -1,13 +1,11 @@
 import sys
-x = int(input("FizzBUzz: Enter an Integer > "))
-y = 1
-while y <= x:
+x = int(input("FizzBUzz: Enter an Integer > ")) + 1
+for y in range(1, x):
     if y % 3 == 0:
         sys.stdout.write("Fizz")
     if y % 5 == 0:
         sys.stdout.write("Buzz")
     if y % 5 != 0 and y % 3 != 0:
         sys.stdout.write(str(y))
-    y += 1
     sys.stdout.write("\n")
 input()
