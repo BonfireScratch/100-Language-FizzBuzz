c0b3530a1da2d2b0e4b5aec12e834652129b9cfe
Dandigit <37099129+Dandigit@users.noreply.github.com>
2018-12-07 16:21:01 +1000

    Create fizzbuzz.rs

---
+++ rust/fizzbuzz.rs
@@ -0,0 +1,35 @@
+// Compile with rustc fizzbuzz.rs
+
+use std::io;
+use std::io::Write;
+
+fn fizz_buzz(min: i32, max: i32) {
+    for current in min..max + 1 {
+        match (current % 5 == 0, current % 3 == 0) {
+            (true, true) => println!("FizzBuzz"),
+            (false, true) => println!("Buzz"),
+            (true, false) => println!("Fizz"),
+            (false, false) => println!("{}", current),
+        }
+    }
+}
+
+fn main() {
+    loop {
+        print!("FizzBuzz: Enter an integer > ");
+        io::stdout().flush().unwrap();
+
+        let mut number = String::new();
+
+        io::stdin()
+            .read_line(&mut number)
+            .expect("Could not read line.");
+        
+        let number = match number.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
+
+        fizz_buzz(1, number);
+    }
+}
