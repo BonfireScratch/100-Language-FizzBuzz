3f3cb4ffbfe411cd8ac7001239ce1af922321a6f
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2018-12-10 14:23:51 +0000

    Update JsFizzBuzz.js

--- web-js/JsFizzBuzz.js
+++ web-js/JsFizzBuzz.js
@@ -1,15 +1,17 @@
-let x = prompt("Fizzbuzz - Enter an Integer > ");
-let y = 1;
-while (y <= x) {
-    if (y % 3 == 0 && y % 5 != 0) {
-        document.write("Fizz");
-    } 
-    if (y % 3 != 0 && y % 5 == 0) {
-        document.write("Buzz");
+while (True) {
+    let x = prompt("Fizzbuzz - Enter an Integer > ");
+    let y = 1;
+    while (y <= x) {
+        if (y % 3 == 0 && y % 5 != 0) {
+            document.write("Fizz");
+        } 
+        if (y % 3 != 0 && y % 5 == 0) {
+            document.write("Buzz");
+        }
+        if (y % 3 != 0 && y % 5 != 0) {
+            document.write(y);
+        } 
+        y++;
+        document.write(" ")
     }
-    if (y % 3 != 0 && y % 5 != 0) {
-        document.write(y);
-    } 
-    y++;
-    document.write(" ")
 }
