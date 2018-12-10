8a291fee5538a08aa5884dfdee46f63bfb1b5bea
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2018-12-10 14:13:50 +0000

    Update fizzbuzz.swift

--- fizzbuzz.swift
+++ fizzbuzz.swift
@@ -2,16 +2,15 @@ print("Enter an integer")
 let response = readLine()
 let x = response.toInt() 
 for i in 1...x {
-    if i % 15 == 0 {
-        print("FizzBuzz")
-    }
-    else if (i % 3 == 0) {
-        print("Fizz")
-    }
-    else if (i % 5 == 0) {
-        print("Buzz")
-    }
-    else {
-        print(i)
-    }
+  var result = ""
+  if i % 3 == 0 {
+    result += "Fizz"
+  }
+  if i % 5 == 0 {
+    result += (result.isEmpty ? "" : " ") + "Buzz"
+  }
+  if result.isEmpty {
+  result += "\(i)"
+  }
+  print(result)
 }
